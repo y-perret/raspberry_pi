95207e2694c9f9559eaf86753afb2ec5e8231219
Yann Perret <y.perret94@gmail.com>
2019-09-10 20:23:48 +0200

    Improve pan tilt fluidity

--- pantilt_controller.py
+++ pantilt_controller.py
@@ -7,19 +7,25 @@ class PanTiltController:
     min_duty_cycle = 2
     max_duty_cycle = 12
 
+    #Initial values for each angles
+    pan_angle = 90
+    tilt_angle = 90
+
     # Initializer / Instance Attributes
-    def __init__(self, pan_pin=3, tilt_pin=5, frequency=50, sleep_time = 0.5):
+    def __init__(self, pan_pin=3, tilt_pin=5, frequency=50, sleep_time = 0.5, movement_threshold = 20):
         """
         A Pan / Tilt controller is used to control the two servos to create a rotation on two axis
         :param pan_pin: GPIO pin associated to the servo used for panning
         :param tilt_pin: GPIO pin associated to the servo used for tilting
         :param frequency: frequency used by the servos
         :param sleep_time: time between each movement
+        :param movement_threshold: threshold in degree of the difference of angles to allow a movement. This reduces small unwanted movements
         """
         self.pan_pin = pan_pin
         self.tilt_pin = tilt_pin
         self.frequency = frequency
         self.sleep_time = sleep_time
+        self.movement_threshold = movement_threshold
 
         #Sets the pin names to board mode
         GPIO.setmode(GPIO.BOARD)
@@ -37,14 +43,6 @@ class PanTiltController:
         self.pwm1.start(0)
         self.pwm2.start(0)
 
-    def stop(self):
-        """
-        Stop the PWMs and clean the IOs
-        """
-        self.pwm1.stop()
-        self.pwm2.stop()
-        GPIO.cleanup()
-
     def pan(self, angle):
         """
         Pan the camera to specific angle
@@ -52,13 +50,15 @@ class PanTiltController:
         """
         #Clamp between 0 and 180
         angle = max(0, min(angle, 180))
-        # Compute the duty cycle in % corresponding to the angle
-        duty_cycle = angle * (self.max_duty_cycle - self.min_duty_cycle) / 180 + self.min_duty_cycle
-        GPIO.output(self.pan_pin, True)
-        self.pwm1.ChangeDutyCycle(duty_cycle)
-        sleep(self.sleep_time)
-        GPIO.output(self.pan_pin, False)
-        self.pwm1.ChangeDutyCycle(0)
+        # Move only if the difference is significant
+        #print(angle)
+        if abs(angle - self.pan_angle) > self.movement_threshold:
+            # Compute the duty cycle in % corresponding to the angle
+            duty_cycle = angle * (self.max_duty_cycle - self.min_duty_cycle) / 180 + self.min_duty_cycle
+            self.pwm1.ChangeDutyCycle(duty_cycle)
+            sleep(self.sleep_time)
+            #self.pwm1.ChangeDutyCycle(0)
+            self.pan_angle = angle
 
     def tilt(self, angle):
         """
@@ -67,13 +67,24 @@ class PanTiltController:
         """
         # Clamp between 30 and 150
         angle = max(30, min(angle, 150))
-        #Compute the duty cycle in % corresponding to the angle
-        duty_cycle = angle * (self.max_duty_cycle - self.min_duty_cycle) / 180 + self.min_duty_cycle
-        GPIO.output(self.tilt_pin, True)
-        self.pwm2.ChangeDutyCycle(duty_cycle)
-        sleep(self.sleep_time)
-        GPIO.output(self.tilt_pin, False)
-        self.pwm2.ChangeDutyCycle(0)
+        # Move only if the difference is significant
+        if abs(angle - self.tilt_angle) > self.movement_threshold:
+            #Compute the duty cycle in % corresponding to the angle
+            duty_cycle = angle * (self.max_duty_cycle - self.min_duty_cycle) / 180 + self.min_duty_cycle
+            self.pwm2.ChangeDutyCycle(duty_cycle)
+            sleep(self.sleep_time)
+            #self.pwm2.ChangeDutyCycle(0)
+            self.tilt_angle = angle
+
+    def stop(self):
+        """
+        Stop the PWMs and clean the IOs
+        """
+        self.pan(90)
+        self.tilt(90)
+        self.pwm1.stop()
+        self.pwm2.stop()
+        GPIO.cleanup()
 
 
 
